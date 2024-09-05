fc0e780db02466ee6bb621cb3f33296cb381a03a
Sanjay Karki <karkisanjay2002@gmail.com>
2024-09-05 12:52:16 +0545

    Error:

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound= () => {
     <div className='flex items-center justify-center min-h-screen'>
         <div className='flex flex-col gap-2 text-center'>
         <h1 className='text-xl font-bold'>404 - Page Not Found</h1>
-        <p className='text-gray-700'>The page you're looking for does not exist.</p>
+        <p className='text-gray-700'>The page you are looking for does not exist.</p>
         </div>
      
     </div>
