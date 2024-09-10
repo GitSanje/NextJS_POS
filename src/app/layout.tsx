5f11d72474b745561d4e933b2fca7ceb128b5831
Sanjay Karki <karkisanjay2002@gmail.com>
2024-09-10 22:17:37 +0545

    Cannot find module './auth/profile/page'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 
 import { Metadata } from 'next';
-import page from "./auth/profile/page";
+
 import { Providers } from "./providers";
 import { ToastContainer } from "react-toastify";
 const inter = Inter({ subsets: ["latin"] });
